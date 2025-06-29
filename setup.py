c12f861327ebe84f9dd9e4c608fa34e375653291
Sharan Ganti <gantisharan6639@gmail.com>
2025-06-29 21:04:20 +0530

    Template is ready and also setup files for requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sharan Ganti",
+    author_email="gantisharan6639@gmail.com",
+    packages=find_packages()
+)
